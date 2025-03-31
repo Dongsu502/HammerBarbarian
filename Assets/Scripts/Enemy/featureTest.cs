861f084013377d14a2dc044d9078c9abd2aafc02
박동수 <qkrehdtns3410@naver.com>
2025-04-01 01:46:43 +0900

    Branch test

---
+++ Assets/Scripts/Enemy/featureTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class featureTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("test");
+    }
+}
