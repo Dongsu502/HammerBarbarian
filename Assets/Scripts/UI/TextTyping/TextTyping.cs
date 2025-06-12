c1189f0d517ccf0b8f93d6e463b87ba18f927b77
박동수 <qkrehdtns3410@naver.com>
2025-06-13 01:55:49 +0900

    UI 자잘한 수정, 타이핑 수정

--- Assets/Scripts/UI/TextTyping/TextTyping.cs
+++ Assets/Scripts/UI/TextTyping/TextTyping.cs
@@ -6,6 +6,62 @@ using UnityEngine.UI;
 
 public class TextTyping : MonoBehaviour
 {
+    public bool IsTyping => isTyping;
+
+    private Coroutine typingCoroutine;
+    private bool isTyping = false;
+
+    private Text currentText;
+    private string fullText;
+
+    public void StartTyping(Text uiText, string newText, float speed)
+    {
+        //이미 실행중이라면 멈추고 새로 시작
+        StopTyping();
+
+        fullText = newText;
+        currentText = uiText;
+
+        typingCoroutine = StartCoroutine(TypingCoroutine(speed));
+    }
+
+    public void SkipTyping()
+    {
+        if (!isTyping) return;
+
+        StopTyping();
+        currentText.text = fullText;
+    }
+
+    private IEnumerator TypingCoroutine(float speed)
+    {
+        isTyping = true;
+        currentText.text = "";
+
+        StringBuilder sb = new StringBuilder();
+
+        foreach(char c in fullText)
+        {
+            sb.Append(c);
+            currentText.text = sb.ToString();
+            
+            yield return new WaitForSecondsRealtime(speed);
+        }
+
+        isTyping = false;
+    }
+
+    private void StopTyping()
+    {
+        if(typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+            typingCoroutine = null;
+        }
+
+        isTyping= false;
+    }
+
     /// <summary>
     /// 타이핑효과
     /// </summary>
@@ -13,19 +69,24 @@ public class TextTyping : MonoBehaviour
     /// <param name="_newString">텍스트변수에 들어갈 값</param>
     /// <param name="typingSpeed">타이핑 속도</param>
     /// <returns></returns>
-    public IEnumerator Typing(Text _newText, string _newString, float typingSpeed)
-    {
-        _newText.text = string.Empty;
+    //public IEnumerator Typing(Text _newText, string _newString, float typingSpeed)
+    //{
+    //    isTyping = true;
+    //    yield return null;
 
-        StringBuilder stringBuilder = new StringBuilder();
+    //    _newText.text = string.Empty;
+
+    //    StringBuilder stringBuilder = new StringBuilder();
         
-        for(int i = 0; i < _newString.Length; i++)
-        {
-            stringBuilder.Append(_newString[i]);
-            _newText.text = stringBuilder.ToString();
+    //    for(int i = 0; i < _newString.Length; i++)
+    //    {
+    //        stringBuilder.Append(_newString[i]);
+    //        _newText.text = stringBuilder.ToString();
 
-            //속도
-            yield return new WaitForSecondsRealtime(typingSpeed);
-        }
-    }
+    //        //속도
+    //        yield return new WaitForSecondsRealtime(typingSpeed);
+    //    }
+    //    yield return null;
+    //    isTyping = false;
+    //}
 }
